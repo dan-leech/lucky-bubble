75f84bac5eacef45ad0604b1d43e89562db545f6
Daniil Kostin <dd@dgr.ru>
2018-07-15 21:21:31 +0300

    D. Fix errors in original version
    call defaultLifecycle.setResult (as described in http://aq-miniapp-core-js-library.readthedocs.io/en/latest/integration_guide.html#call-lifecycle-informready)
    call defaultLifecycle.end when the game has ended (use same sdk documentation)

--- src/SampleGame.js
+++ src/SampleGame.js
@@ -4,7 +4,7 @@ import {
 } from 'aq-miniapp-core';
 import Game from './components/Game';
 import Assets, {ASSETS} from './assets';
-import TextureAnimator from './utils/TextureAnimator2';
+import TextureAnimator from './utils/TextureAnimator';
 import DeviceOrientationControls from './utils/DeviceOrientationControls';
 
 
@@ -85,8 +85,8 @@ let clock = new THREE.Clock();
 
 let mouseCoords = new THREE.Vector2();
 let raycaster = new THREE.Raycaster();
-let bubbleColor = 0x00ff00;
-let frame = new THREE.NodeFrame();
+const bubbleColor = 0x09D539;
+const bubbleOpacity = 0.9
 
 const bubbleFadeSpeed = 2;
 const burstSpeed = 20;
@@ -100,10 +100,9 @@ let isGameStartScreen = false;
 let gameTriesLeft = 0;
 let gameWinTotal = 0;
 let gameTimer = null;
+const gameTriesTotal = 20; // total tries to win - random 5
 const gameTime = 10; // seconds
 
-let boomer = null;
-var runner = null, runnerMaterial = null;
 let mobileBrowser = false;
 
 if ( navigator.userAgent.match( /Android/i )
@@ -132,7 +131,6 @@ const inputScale = 5000;
 const inputCameraSpeed = 0.01;
 let inputBeta = 0;
 let inputGamma = 0;
-var runnerTexture  = null;
 
 
 export default class SampleGame extends Game<Props> {
@@ -146,7 +144,6 @@ bubbleTexture: THREE.Texture;
 playTexture: THREE.Texture;
 emoticonsTextures: [];
 coins: [];
-burstAnimator: THREE.TextureAnimator;
 material: THREE.Material;
 camera: THREE.Camera;
 deviceControls: THREE.DeviceOrientationControls;
@@ -161,14 +158,13 @@ gameDidMount() {
   // will be called
   this.playTexture = new THREE.TextureLoader(this.loadingManager).load(Assets.textures.playBtn);
   this.alphaTexture = new THREE.TextureLoader(this.loadingManager).load(Assets.textures.whitePixel);
-  this.burstTexture = new THREE.TextureLoader(this.loadingManager).load(this.props.engagementInfo.bubblePopped);
+  this.burstTexture = new THREE.TextureLoader(this.loadingManager).load(this.props.engagementInfo.popAnimation);
 
   this.cubeTexture = new THREE.CubeTextureLoader(this.loadingManager).load(this.props.engagementInfo.cubeBackground);
 
 }
 
 assetsDidLoad() {
-  runnerTexture  = new THREE.ImageUtils.loadTexture(this.props.engagementInfo.popAnimation )
   this._initGraphics();
   this._initInput();
   this._initGui();
@@ -260,110 +256,6 @@ _loadOtherAssets() {
   });
 }
 
-ExplodeAnimation(position, startScreen) {
-  if(mobileBrowser) {
-    if(gameTriesLeft === 3)
-      boomer = new this.TextureAnimator(runnerTexture, 19, 1, 19, 22); // texture, #horiz, #vert, #total, duration.
-    else {
-      boomer = new this.TextureAnimator(runnerTexture, 19, 1, 19, 14); // texture, #horiz, #vert, #total, duration.
-    }
-  }
-  else {
-    if(gameTriesLeft === 3)
-      boomer = new this.TextureAnimator(runnerTexture, 19, 1, 19, 14); // texture, #horiz, #vert, #total, duration.
-    else
-      boomer = new this.TextureAnimator(runnerTexture, 19, 1, 19, 4); // texture, #horiz, #vert, #total, duration.
-  }
-  runnerMaterial = new THREE.MeshBasicMaterial( { map: runnerTexture, transparent:true  } );
-  var runnerGeometry = null
-  if (startScreen) {
-    runnerGeometry = new THREE.PlaneGeometry(250, 250, 1, 1);
-  } else {
-    runnerGeometry = new THREE.PlaneGeometry(850, 850, 1, 1);
-  }
-  runner = new THREE.Mesh(runnerGeometry, runnerMaterial);
-  runner.position.copy(position);
-  if (startScreen) {
-    this.sceneCube.add(runner);
-    //runner.lookAt(this.cameraCube.position);
-  } else {
-    this.scene.add(runner);
-    runner.lookAt(this.camera.position);
-  }
-}
-
-TextureAnimator(texture, tilesHoriz, tilesVert, numTiles, tileDispDuration)
-{
-  // note: texture passed by reference, will be updated by the update function.
-  var textureCounter = 0;
-  this.tilesHorizontal = tilesHoriz;
-  this.tilesVertical = tilesVert;
-  // how many images does this spritesheet contain?
-  //  usually equals tilesHoriz * tilesVert, but not necessarily,
-  //  if there at blank tiles at the bottom of the spritesheet.
-  this.numberOfTiles = numTiles;
-  texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-  texture.repeat.set( 1 / this.tilesHorizontal, 1 / this.tilesVertical );
-  // how long should each image be displayed?
-  this.tileDisplayDuration = tileDispDuration;
-  // how long has the current image been displayed?
-  this.currentDisplayTime = 0;
-  // which image is currently being displayed?
-  this.currentTile = 0;
-
-  this.update = function( milliSec )
-  {
-    this.currentDisplayTime += milliSec;
-    while (this.currentDisplayTime > this.tileDisplayDuration)
-    {
-      this.currentDisplayTime -= this.tileDisplayDuration;
-      if(mobileBrowser) {
-        if(this.currentTile === 2) {
-          textureCounter++;
-        }
-        if(gameTriesLeft === 3) {
-          if (this.currentTile < 16) {
-            if(!(textureCounter < 4 && this.currentTile === 2))
-              this.currentTile++;
-          }
-        }
-        else {
-          if (this.currentTile < 18) {
-            if(!(textureCounter < 3 && this.currentTile === 2))
-              this.currentTile++;
-          }
-        }
-      }
-      else {
-        if(this.currentTile === 2) {
-          textureCounter++;
-        }
-
-        if (this.currentTile < 18) {
-          if(!(textureCounter < 3 && this.currentTile === 2))
-            this.currentTile++;
-        }
-      }
-      //if(gameTriesLeft === 3) {
-      //
-      //  if (this.currentTile == this.numberOfTiles)
-      //    this.currentTile = 0;
-      //}
-      var currentColumn = this.currentTile % this.tilesHorizontal;
-      texture.offset.x = currentColumn / this.tilesHorizontal;
-      var currentRow = Math.floor( this.currentTile / this.tilesHorizontal );
-      texture.offset.y = currentRow / this.tilesVertical;
-    }
-  };
-}
-
-updateme()
-{
-  var delta = clock.getDelta();
-  if(boomer !== null)
-    boomer.update(1000 * delta);
-}
-
 _onTouchBubble(scope, event) {
   let x = event.clientX || (event.touches ? event.touches[0].pageX : 0);
   let y = event.clientY || (event.touches ? event.touches[0].pageY : 0);
@@ -373,66 +265,62 @@ _onTouchBubble(scope, event) {
   );
 
   let intersections;
-  var me = this;
 
   if (isGameRunning) {
     raycaster.setFromCamera(mouseCoords, scope.camera);
-    intersections = raycaster.intersectObjects(bubbles);
+    intersections = raycaster.intersectObjects(bubbles.filter(item => !item._isPoping));
 
     if (intersections.length > 0) {
       let intersected = intersections[0].object;
+      if (intersected._isPoping || (scope.props.shouldWin && gameTriesLeft <= 0)) return;
       intersected._isPoping = true;
-      // let spriteMap;
-      // if (Math.random() <= gameWinProbability) {
-      //   let coin = scope.coins[Math.ceil(Math.random() * scope.coins.length - 1)];
-      //   spriteMap = coin.texture;
-      //   gameWinTotal += coin.value;
-      // } else
-      //   spriteMap = scope.emoticonsTextures[Math.ceil(Math.random() * scope.emoticonsTextures.length - 1)];
-
-      // todo: coin on win
-      // let spriteMaterial = new THREE.SpriteMaterial({map: spriteMap, color: 0xffffff});
-      // scope.billboard = new THREE.Sprite(spriteMaterial);
-      // scope.billboard.position.copy(intersected.position);
-      // scope.billboard.scale.x = scope.billboard.scale.y = 750;
 
       gameTriesLeft--;
       function checkGameOver() {
-        if (!gameTriesLeft) {
+        if (scope.props.shouldWin && gameTriesLeft <= 0) {
           scope._guiGameOver()
         }
       }
 
+      function showCoin() {
+        if(scope.props.shouldWin && gameTriesLeft === 0) {
+          let spriteMap;
+          let coin = scope.coins[Math.ceil(Math.random() * scope.coins.length - 1)];
+          spriteMap = coin.texture;
+
+          let spriteMaterial = new THREE.SpriteMaterial({map: spriteMap, color: 0xffffff});
+          scope.billboard = new THREE.Sprite(spriteMaterial);
+          scope.billboard.position.copy(intersected.position);
+          scope.billboard.scale.x = scope.billboard.scale.y = 750;
+          scope.scene.add(scope.billboard);
+
+          return scope.billboard;
+        }
+
+        return null;
+      }
+
       intersected._animator
-          .onComplete(function (animator, texture) {
+          .onComplete(function () {
             intersected._isPoped = true;
 
-            var self = me;
-            self.ExplodeAnimation(intersected.position);
+            let billboard = showCoin()
 
             if(mobileBrowser) {
               setTimeout(function () {
-                me.scene.remove(runner);
-                boomer = null;
-                runner = null;
-                runnerMaterial = null;
+                scope._removeSceneObject(billboard);
                 checkGameOver();
-                scope._removeSceneObject(scope.billboard);
               }, 900);
             }
             else {
               setTimeout(function () {
-                me.scene.remove(runner);
-                boomer = null;
-                runner = null;
-                runnerMaterial = null;
+                scope._removeSceneObject(billboard);
                 checkGameOver();
-                scope._removeSceneObject(scope.billboard);
               }, 400);
             }
           })
           .reset()
-          .start();
+          .start(intersected.position);
 
     }
   } else {
@@ -444,34 +332,15 @@ _onTouchBubble(scope, event) {
     if (intersections.length > 0) {
       isGameStartScreen = false;
       let intersected = intersections[0].object;
+      if (intersected._isPoping) return;
       intersected._isPoping = true;
 
       intersected._animator
-          .onComplete(function (animator) {
+          .onComplete(function () {
             intersected._isPoped = true;
-
-            var self = me;
-            self.ExplodeAnimation(intersected.position, true);
-
-            if(mobileBrowser) {
-              setTimeout(function () {
-                me.sceneCube.remove(runner);
-                boomer = null;
-                runner = null;
-                runnerMaterial = null;
-              }, 900);
-            }
-            else {
-              setTimeout(function () {
-                me.sceneCube.remove(runner);
-                boomer = null;
-                runner = null;
-                runnerMaterial = null;
-              }, 400);
-            }
           })
           .reset()
-          .start();
+          .start(intersected.position);
     }
   }
 }
@@ -480,7 +349,7 @@ _startGame() {
   let scope = this;
 
   if (this.props.shouldWin) {
-    gameTriesLeft = Math.ceil(20 - Math.random() * 5);
+    gameTriesLeft = Math.ceil(gameTriesTotal - Math.random() * 5);
   } else {
     gameTimer = gameTime
   }
@@ -506,28 +375,24 @@ _guiGameOver() {
     guiGameOver.style.display = 'none';
     guiCenterContainer.style.display = 'flex';
 
+    // END of the GAME
     if (scope.props.shouldWin) {
       guiWin.style.display = 'block';
-      // END of the GAME
-
-      defaultLifeCycle.setResult({
-        winCriteriaPassed: gameWinTotal > 0,
-        score: {
-          value: gameWinTotal
-        },
-        resultImageUrl: this.props.engagementInfo.background
-      });
-
-      setTimeout(() => {
-        defaultLifeCycle.end();
-      }, 2000);
     } else {
       guiGameOver.style.display = 'block';
-
-      setTimeout(() => {
-        defaultLifeCycle.end();
-      }, 2000);
     }
+
+    defaultLifeCycle.setResult({
+      winCriteria: scope.props.shouldWin,
+      score: {
+        value: gameWinTotal
+      },
+      resultImageUrl: this.props.engagementInfo.background
+    });
+
+    setTimeout(() => {
+      defaultLifeCycle.end();
+    }, 2000);
   }).bind(this), 1000);
 }
 
@@ -596,20 +461,12 @@ _createStartButton() {
           THREE.Math3Node.MIX
       );
 
-  let animator = new THREE.TextureAnimator(this.burstTexture, burstSpeed, 2, 0.2, false);
-  //mtl.alpha = animator.alphaNode;
+  let animator = new THREE.TextureAnimator(this.burstTexture, this.sceneCube, this.cameraCube, 19, 1, 19, burstSpeed);
 
   // build shader
   mtl.build();
-  let mesh = new THREE.Mesh(this.geometry, mtl);
   this.startBubble = new THREE.Mesh(this.geometry, mtl);
   this.sceneCube.add(this.startBubble);
-
-  // let spriteMap = this.playTexture;
-  // let spriteMaterial = new THREE.SpriteMaterial({map: spriteMap, color: 0xffffff});
-  // this.startBubblePlay = new THREE.Sprite(spriteMaterial);
-  // this.startBubblePlay.scale.x = this.startBubblePlay.scale.y = 60;
-  //this.sceneCube.add(this.startBubblePlay);
 
   this.startBubble._animator = animator;
 
@@ -619,13 +476,11 @@ _createStartButton() {
 _createBubbles() {
   bubbles = [];
 
-  var mtl = new THREE.MeshBasicMaterial( { color: 0x09D539, envMap: this.cubeTexture, opacity:0.9, wireframe:false, transparent:true } );
+  var mtl = new THREE.MeshBasicMaterial( { color: bubbleColor, envMap: this.cubeTexture, opacity: bubbleOpacity, wireframe:false, transparent:true } );
 
   for (let i = 0; i < 200; i++) {
-    let animator = new THREE.TextureAnimator(this.burstTexture, burstSpeed, 2, 0.2, false);
-    mtl.alpha = animator.alphaNode;
+    let animator = new THREE.TextureAnimator(this.burstTexture, this.scene, this.camera, 19, 1, 19, burstSpeed, 850);
     let mesh = new THREE.Mesh(this.geometry, mtl);
-
 
     mesh.position.x = Math.random() * 5000 - 2500;
     mesh.position.y = Math.random() * 5000 - 2500;
@@ -633,7 +488,7 @@ _createBubbles() {
     mesh.scale.x = mesh.scale.y = mesh.scale.z = 4;
     this.scene.add(mesh);
     mesh._index = i;
-    mesh._creating = true;
+    mesh._creating = false;
     mesh._animator = animator;
     bubbles.push(mesh);
   }
@@ -678,8 +533,6 @@ render() {
 
   this.renderer.render(this.sceneCube, this.cameraCube);
   this.renderer.render(this.scene, this.camera);
-  this.updateme();
-
 }
 
 _controlCamera() {
@@ -706,11 +559,11 @@ _trackBubbles(deltaTime) {
 
     this.startBubble._animator.update(deltaTime);
 
-    if (this.startBubble._isPoped) {
+    if (this.startBubble._isPoping) {
       this.sceneCube.remove(this.startBubble);
-      // this.sceneCube.remove(this.startBubblePlay); // already removed on click
-      //this._disposeObjectMaterials(this.startBubble);
-      //this._disposeObjectMaterials(this.startBubblePlay);
+    }
+    if (this.startBubble._isPoped) {
+      this.startBubble._animator.dispose();
       this.startBubble = null;
       this._startGame();
     }
@@ -718,29 +571,28 @@ _trackBubbles(deltaTime) {
 
   for (let i = bubbles.length - 1; i >= 0; i--) {
     let bubble = bubbles[i];
-    if (bubble._isPoped) {
+    if (bubble._isPoping) {
       this._removeSceneObject(bubble);
+    }
+    if (bubble._isPoped) {
+      bubble._animator.dispose();
       bubbles.splice(i, 1);
       continue;
     }
 
     if (bubble._hidding) {
-      let alpha = bubble._animator.alpha;
-      let alpha2 = bubble._animator.maskAlpha;
-      alpha2.number -= bubbleFadeSpeed * deltaTime;
-      alpha.number -= bubbleFadeSpeed * deltaTime;
-      if(alpha.number <= 0) {
-        alpha.number = 0;
-        alpha2.number = 0;
+      bubble.material.opacity -= bubbleFadeSpeed * deltaTime;
+      if(bubble.material.opacity <= 0) {
+        bubble.material.opacity = 0;
         this._removeSceneObject(bubble);
+        bubble._animator.dispose();
         bubbles.splice(i, 1);
         continue;
       }
     } else if (bubble._creating) {
-      let alpha = bubble._animator.alpha;
-      alpha.number += bubbleFadeSpeed * deltaTime;
-      if(alpha.number >= 1) {
-        alpha.number = 1;
+      bubble.material.opacity += bubbleFadeSpeed * deltaTime;
+      if(bubble.material.opacity >= bubbleOpacity) {
+        bubble.material.opacity = bubbleOpacity;
         bubble._creating = false;
       }
     }
---
+++ src/utils/TextureAnimator.js
@@ -0,0 +1,90 @@
+module.exports = function (THREE) {
+
+  THREE.TextureAnimator = function (textureSource, sceneSource, cameraSource, tilesHoriz, tilesVert, numTiles, tileDispDuration, billboardSize) {
+    // note: texture passed by reference, will be updated by the update function.
+    let tilesHorizontal = tilesHoriz;
+    let tilesVertical = tilesVert;
+
+    let size = billboardSize || 250;
+    // how many images does this spritesheet contain?
+    //  usually equals tilesHoriz * tilesVert, but not necessarily,
+    //  if there at blank tiles at the bottom of the spritesheet.
+    let numberOfTiles = numTiles;
+    let texture = textureSource.clone();
+    texture.needsUpdate = true;
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set( 1 / tilesHorizontal, 1 / tilesVertical );
+    // how long should each image be displayed?
+    let tileDisplayDuration = tileDispDuration;
+    // how long has the current image been displayed?
+    let currentDisplayTime = 0;
+    // which image is currently being displayed?
+    let currentTile = 0;
+
+    let material = new THREE.MeshBasicMaterial( { map: texture, transparent:true  } );
+    let geometry = new THREE.PlaneGeometry(size, size, 1, 1);
+    let mesh = new THREE.Mesh(geometry, material);
+
+    // completion callback
+    this.onCompleteCallback = null;
+
+    // running
+    let isRunning = false;
+
+    this.onComplete = function (callback) {
+      this.onCompleteCallback = callback;
+
+      return this;
+    };
+
+    this.start = function (position) {
+      isRunning = true;
+
+      currentTile = 0;
+      currentDisplayTime = 0;
+
+      mesh.position.copy(position);
+
+      sceneSource.add(mesh);
+
+      return this;
+    };
+
+    this.update = function (milliSec) {
+      if (!isRunning) return;
+
+      currentDisplayTime += milliSec * 1000;
+
+      currentTile = Math.ceil(currentDisplayTime / tileDisplayDuration) - 1;
+
+      mesh.lookAt(cameraSource.position);
+
+      if (currentTile < numberOfTiles) {
+        let currentColumn = currentTile % tilesHorizontal;
+        texture.offset.x = currentColumn / tilesHorizontal;
+        let currentRow = Math.floor( currentTile / tilesHorizontal );
+        texture.offset.y = currentRow / tilesVertical;
+      } else {
+        isRunning = false;
+        sceneSource.remove(mesh);
+
+        if (typeof this.onCompleteCallback === 'function')
+          this.onCompleteCallback(this);
+      }
+    };
+
+    this.reset = function () {
+      currentTile = 0;
+      currentDisplayTime = 0;
+
+      sceneSource.remove(mesh);
+
+      return this;
+    };
+
+    this.dispose = function () {
+      material.dispose();
+      geometry.dispose();
+    };
+  }
+};
